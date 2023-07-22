f5bb14209d40afc24091cf8c38fb697eb9b57a46
ATH\anikou <anikou@netcompany-intrasoft.com>
2023-07-22 19:14:36 +0300

    Initial commit

---
+++ src/main/java/open/api/in/spring/boot/OpenApiInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package open.api.in.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiInSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiInSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/open/api/in/spring/boot/OpenApiInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package open.api.in.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiInSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
